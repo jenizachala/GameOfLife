cf35b25bcd73d1d61856aba563e15c988faff16e
Jeniza Chala <52945554+jenizachala@users.noreply.github.com>
2021-06-29 18:58:27 -0500

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,6 @@
+#Example Starting Pattern for Conway's Game of Life
+
+#Infinite Growth Pattern
+gradle run --args='7,25 8,25 9,25 10,25 11,25 12,25 13,25 14,25 16,25 17,25 18,25 19,25 20,25 24,25 25,25 26,25 33,25 34,25 35,25 36,25 37,25 38,25 39,25 41,25 42,25 43,25 44,25 45,25'
+
+#Run by using command: ./run.sh
